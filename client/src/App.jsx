854357d749779f8420323791d4d2e5e277c3a644
ArManiyar <arsufiyan.maniyar@gmail.com>
2023-12-02 04:13:47 +0530

    Added header to all the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/signup";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
